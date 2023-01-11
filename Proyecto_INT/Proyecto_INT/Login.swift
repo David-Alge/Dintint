b1156a5820833c7a638a7073ade93fe5c6c061ea
David-Alge <73697105+David-Alge@users.noreply.github.com>
2023-01-11 11:35:19 +0100

    Commit inicial

---
+++ Proyecto_INT/Proyecto_INT/Login.swift
@@ -0,0 +1,20 @@
+//
+//  Login.swift
+//  Proyecto_INT
+//
+//  Created by David Alge Balaguer on 2/11/22.
+//
+
+import UIKit
+
+class Login: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
---
+++ Proyecto_INT/Proyecto_INT/ZonaMovil.swift
@@ -0,0 +1,105 @@
+//
+//  ZonaMovil.swift
+//  Proyecto_INT
+//
+//  Created by David Alge Balaguer on 30/11/22.
+//
+
+import UIKit
+
+class ZonaMovil: UITableViewController {
+    
+    let arrayDatos = ["movil1","movil2","movil3","movil4"]
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return arrayDatos.count
+    }
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "miCeldaMovil", for: indexPath) as! CeldaMovil
+
+        cell.miLabel.text = arrayDatos [indexPath.row]
+        cell.miImagen.image = UIImage(named:"movil")
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    
+}
+
+class CeldaMovil : UITableViewCell {
+    @IBOutlet weak var miImagen: UIImageView!
+    @IBOutlet weak var miBoton: UIButton!
+    @IBOutlet weak var miLabel: UILabel!
+    
+}
