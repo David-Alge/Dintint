336704d14bbe861fc10e7c482fde08f6234d4f7f
David-Alge <algedavid02@gmail.com>
2023-01-20 09:08:19 +0100

    file manageador

--- Proyecto_INT copia/Proyecto_INT/Login.swift
+++ Proyecto_INT copia/Proyecto_INT/Login.swift
@@ -12,6 +12,8 @@ class Login: UIViewController {
     @IBOutlet weak var txtContra: UITextField!
     @IBOutlet weak var txtUsuario: UITextField!
     @IBOutlet weak var lblalert: UILabel!
+    
+    var datos = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -29,6 +31,15 @@ class Login: UIViewController {
         if txtUsuario.text == ""  || txtContra.text == "" {
             NotificationCenter.default.post(name: NSNotification.Name("Alerta"), object: nil)
         }else{
+            let miTextoAGuardar = txtUsuario.text ?? "No hay texto"
+            
+            let miFileManager = FileManager.default
+            
+            leerDatos()
+            
+            
+            guardarDatos()
+            
             let miStoryboard = UIStoryboard(name: "Main", bundle: nil)
             
             let view = miStoryboard.instantiateViewController(withIdentifier: "TabBarController") as! TabBarController
@@ -44,6 +55,45 @@ class Login: UIViewController {
     @objc func validate(notification: NSNotification){
         lblalert.textColor = .red
     }
+    
+    func getDocumentPath()-> URL{
+        
+        return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first ?? URL(string: "")!
+    }
+    
+    func miRutaArchivo() -> URL {
+        let miPath = getDocumentPath()
+        let miFicheroURL = miPath.appendingPathComponent("Datos.json")
+        
+        return miFicheroURL
+    }
+    func guardarDatos(){
+        do
+        {
+            let misDatosSerializados = try JSONSerialization.data(withJSONObject: datos)
+            
+            try misDatosSerializados.write(to: miRutaArchivo())
+        }
+        catch _ {
+            print("ERROR FATAL DE LECTURA,SIN DATOS")
+        }
+        }
+        
+    @IBAction func Recu(_ sender: Any) {
+        leerDatos()
+        txtUsuario.text = datos.description
+    }
+    
+    func leerDatos(){
+        do{
+            let misDatosLeidos = try Data(contentsOf: miRutaArchivo())
+            
+            datos = try JSONSerialization.jsonObject(with: misDatosLeidos) as! [String] ?? []
+        }
+        catch _ {
+            print("ERROR FATAL DE LECTURA,SIN DATOS")
+        }
+        }
     
 
 }
